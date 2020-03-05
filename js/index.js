f4d2397b1376c7e5954cbbc05f0f97fa99ded4c9
pedddro <peddro@protonmail.com>
2020-03-05 16:34:00 -0300

    Revert "Revert "update v .1""
    This reverts commit 66d3de4eb1870df6c93a85f18418ba1169043510.

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ linkRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(linkRequest.responseText).forEach(function (link) {
                 console.log(link.name);
-                html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"black-text valign-wrapper\" target=\"_blank\"><i class=\"link_icon mdi mdi-" + link.icon + "\" style=\"color: " + link.color + "\"></i>&nbsp; " + link.name + "</a></div>";
+                html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"valign-wrapper\" target=\"_blank\"><i class=\"link_icon mdi mdi-" + link.icon + "\" style=\"color: " + link.color + "\"></i>&nbsp; " + link.name + "</a></div>";
             });
             link.innerHTML = html;
         }
@@ -34,7 +34,7 @@ workRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(workRequest.responseText).forEach(function (work) {
                 console.log(work.name);
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card medium hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visitar -></a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + work.description + "</p></div></div></div>";
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card medium hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visitar -></a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + work.description + "</p></div></div></div>";
             });
             work.innerHTML = html;
         }
@@ -50,7 +50,7 @@ miscRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(miscRequest.responseText).forEach(function (misc) {
                 console.log(misc.name);
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable large\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + misc.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + misc.link + " target=\"_blank\">Visitar</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + misc.description + "</p></div></div></div>";
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable medium\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + misc.image + "></div><div class=\"card-content\"><span class=\"card-title activator\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + misc.link + " target=\"_blank\">Visitar</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + misc.description + "</p></div></div></div>";
             });
             misc.innerHTML = html;
         }
