eff29ef35067a290f9ad8b79344e8cfafd2caae6
pedddro <peddro@protonmail.com>
2021-01-06 20:23:19 -0300

    lazyload and remove firebase

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ workRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(workRequest.responseText).forEach(function (work) {
                 console.log(work.name);
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card medium hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visitar -></a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + work.description + "</p></div></div></div>";
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card medium hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator lazyload\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\">Visitar -></a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + work.description + "</p></div></div></div>";
             });
             work.innerHTML = html;
         }
@@ -50,7 +50,7 @@ miscRequest.onreadystatechange = function () {
             var html = "";
             JSON.parse(miscRequest.responseText).forEach(function (misc) {
                 console.log(misc.name);
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable medium\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + misc.image + "></div><div class=\"card-content\"><span class=\"card-title activator\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + misc.link + " target=\"_blank\">Visitar</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + misc.description + "</p></div></div></div>";
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable medium\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator lazyload\" src=" + misc.image + "></div><div class=\"card-content\"><span class=\"card-title activator\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + misc.link + " target=\"_blank\">Visitar</a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + misc.name + "<i class=\"material-icons right\">keyboard_arrow_down</i></span><p>" + misc.description + "</p></div></div></div>";
             });
             misc.innerHTML = html;
         }
